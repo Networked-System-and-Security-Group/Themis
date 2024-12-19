cca13d203f1ab7960b0b527d46f4bf419c594cb0
ZXC <1023659154@qq.com>
2024-12-20 02:21:27 +0800

    todo:增加减速后的恢复机制

--- Alibaba-HPCC/simulation/src/point-to-point/model/switch-node.h
+++ Alibaba-HPCC/simulation/src/point-to-point/model/switch-node.h
@@ -38,6 +38,8 @@ class SwitchNode : public Node{
     	Time finish_time;//
     	uint32_t first;//第一个序列号
     	uint32_t biggest;//最大序列号
+		uint32_t group_size;//zxc：用于恢复机制
+		uint32_t count;//zxc：用于恢复机制
     	uint32_t n;//需要resubmit n遍
     	Time delay;//第一个包resubmit带来的延迟
     	bool finished;// n次resubmit是否完成
@@ -48,9 +50,11 @@ class SwitchNode : public Node{
       		delay = Time(0);
       		first = 0;
       		biggest = 0;
-      		n = 5;
+      		n = num;
       		finished = false;
       		sended = true;
+			group_size = 50;
+			count = group_size;
     	}
  	};
 
