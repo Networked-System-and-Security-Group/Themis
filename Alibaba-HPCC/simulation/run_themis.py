85540dc74c569b5e09d90470cde422a26186e339
Zhongguuu <zihanniu85@gmail.com>
2025-01-12 14:01:41 +0000

    checkpoint: good expr results

--- Alibaba-HPCC/simulation/run_themis.py
+++ Alibaba-HPCC/simulation/run_themis.py
@@ -11,9 +11,9 @@ def modify_config(base_config_path, new_config_path, xxx):
     with open(base_config_path, 'r') as file:
         lines = file.readlines()
 
-    lines[6] = f"FLOW_FILE mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Ali_{xxx}_1:10.txt\n"
-    lines[7] = f"FCT_OUTPUT_FILE mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Ali_{xxx}/fct.txt\n"
-    lines[8] = f"PFC_OUTPUT_FILE mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Ali_{xxx}/pfc.txt\n"
+    lines[6] = f"FLOW_FILE mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Web_{xxx}_1:10.txt\n"
+    lines[7] = f"FCT_OUTPUT_FILE mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Web_{xxx}/fct.txt\n"
+    lines[8] = f"PFC_OUTPUT_FILE mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Web_{xxx}/pfc.txt\n"
     lines[9] = f"TRACE_FILE mix/trace.txt\n"
     lines[10] = f"TRACE_OUTPUT_FILE mix/mix.tr\n"
 
@@ -24,19 +24,19 @@ def modify_config(base_config_path, new_config_path, xxx):
 def run_simulation(xxx):
     """Run the entire simulation pipeline for a given xxx value."""
     base_config_path = "./mix/config.txt"
-    new_config_path = f"./mix/config_Ali_{xxx}.txt"
+    new_config_path = f"./mix/config_Web_{xxx}.txt"
 
     # Step 1: Generate flow file
     flow_file_cmd = (
-        f"python ../traffic_gen/pure_near_dst.py -c AliStorage2019.txt -n 32 -l {xxx} "
-        f"-b 100G -t 0.03 -o ./mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Ali_{xxx}.txt"
+        f"python ../traffic_gen/pure_near_dst.py -c ../traffic_gen/WebSearch_distribution.txt -n 32 -l {xxx} "
+        f"-b 100G -t 0.03 -o ./mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Web_{xxx}_1:10.txt"
     )
     # execute_command(flow_file_cmd)
 
     # Step 2: Trim the flow file
     trim_cmd = (
         f"python3 ../traffic_gen/trim-pure-dst.py -o "
-        f"./mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Ali_{xxx}.txt"
+        f"./mix/UnfairPenalty/Inter-DC/ExprGroup/flow_Web_{xxx}_1:10.txt"
     )
     # execute_command(trim_cmd)
 
